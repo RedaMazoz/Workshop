a7d1a58e6f5a3aaeef7cdfdcc8475cd51b39aadf
Reda Mazoz <69761147+RedaMazoz@users.noreply.github.com>
2021-04-26 17:10:07 +0100

    Multiple Objects Drag, AutoUpdatable Window and Saves, and Lot of BugFixes

--- Editor/Workshop.cs
+++ Editor/Workshop.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+
 public class Workshop : EditorWindow
 {
     [MenuItem("My Custom Widgets/Workshop")]
@@ -11,10 +12,10 @@ public class Workshop : EditorWindow
     public GUIStyle addButtonGUIStyle;
     public GUIStyle onButtonGUIStyle;
     public GUIStyle offButtonGUIStyle;
-    public GUIStyle clearButtonGUIStyle;
+    public GUIStyle clearButtonGUISt    yle;
 
     [SerializeField] private static List<string> tContainer = new List<string>();
-    private Dictionary <string,GameObject> inactiveElements = new Dictionary<string, GameObject>();
+    [SerializeField] private Dictionary <string,GameObject> inactiveElements = new Dictionary<string, GameObject>();
 
     private bool isAdding = false;
     private int tempInt = 0;
@@ -25,16 +26,19 @@ public class Workshop : EditorWindow
     private List<string> savesNameList = new List<string>();
     private int LastIndex = 0;
     private int saveIndex = 0;
-    
-    private Vector2 scrollPosition;
 
     Dictionary<string,GameObject> sceneInactiveGO = new Dictionary<string, GameObject>();
+
+    private Vector2 scrollPosition;
+
+    private string lastSaveGUID;
 
     // Bug Fix: Empty First Save after Build
     // Putting On Window Reset Loading in OnEnable instead of Awake
     private void OnEnable()
     {
         saveList = AssetDatabase.FindAssets("t: WorkshopElementsScriptableObj");
+        if(saveList.Length == 0) { CreateSaveIfNot(); } //BugFix: Buttons Disappear when All Saves Deleted
         defaultSave = AssetDatabase.LoadAssetAtPath<WorkshopElementsScriptableObj>(AssetDatabase.GUIDToAssetPath(saveList[saveIndex]));
 
         for (int i = 0; i < saveList.Length; i++)
@@ -50,13 +54,39 @@ public class Workshop : EditorWindow
         }
     }
 
+    private void OnProjectChange()
+    {
+        //BugFix: No Update of SaveList when New Save Created
+        if (saveList.Length != 0) { lastSaveGUID = saveList[saveIndex];}
+        saveList = AssetDatabase.FindAssets("t: WorkshopElementsScriptableObj");
+
+        saveIndex = ArrayUtility.IndexOf<string>(saveList, lastSaveGUID);
+        LastIndex = saveIndex;
+
+        savesNameList.Clear();
+        for (int i = 0; i < saveList.Length; i++)
+        {
+            savesNameList.Add(System.Text.RegularExpressions.Regex.Replace(AssetDatabase.GUIDToAssetPath(saveList[i]), "([0-9A-z ]*/)*", "").Replace(".asset", ""));
+        }
+    }
+    private void OnInspectorUpdate()
+    {
+        //BugFix: Active GameObject disappear when desactivated in Inspector after the Worshop was already Open
+        //BugFix: Instant Buttons Color Change if Activated or Desactivated in Inspector
+        GetSceneInactiveGameObjs(sceneInactiveGO);
+        foreach (string goName in tContainer)
+        {
+            if (sceneInactiveGO.ContainsKey(goName) && !inactiveElements.ContainsKey(goName)) { inactiveElements[goName] = sceneInactiveGO[goName];}
+        }
+        Repaint();
+    }
+
     public void Awake()
     {
         addButtonGUIStyle = new GUIStyle();
         addButtonGUIStyle.font = myfont;
         addButtonGUIStyle.normal.textColor = Color.white;
         addButtonGUIStyle.fontSize = 30;
-        
         onButtonGUIStyle = new GUIStyle();
         onButtonGUIStyle.font = myfont;
         onButtonGUIStyle.normal.textColor = Color.white;
@@ -70,7 +100,7 @@ public class Workshop : EditorWindow
         offButtonGUIStyle.fontSize = 20;
         offButtonGUIStyle.fontStyle = FontStyle.Bold;
         offButtonGUIStyle.alignment = TextAnchor.MiddleCenter;
-        
+
         clearButtonGUIStyle = new GUIStyle();
         clearButtonGUIStyle.font = myfont;
         clearButtonGUIStyle.normal.textColor = Color.red;
@@ -90,9 +120,24 @@ public class Workshop : EditorWindow
     }
     private void OnGUI()
     {
-        if (Screen.height / 12 <= 50 && Screen.height / 12 >= 20) { addButtonGUIStyle.fontSize = Screen.height / 12; }
+
         
         saveIndex = EditorGUI.Popup(new Rect(45, 2, 100, 25), saveIndex, savesNameList.ToArray());
+        //BugFix: Buttons Disappear when a currently open Save is Deleted
+        if (defaultSave == null)
+        {
+            tContainer.Clear();
+            saveIndex = 0;
+            LastIndex = 0;
+            if (savesNameList.Count != 0)
+            {
+                //Reset To First Element in List
+                defaultSave = AssetDatabase.LoadAssetAtPath<WorkshopElementsScriptableObj>(AssetDatabase.GUIDToAssetPath(saveList[saveIndex]));
+                tContainer = defaultSave.stContainer;
+                inactiveElements = defaultSave.sInactiveElements;
+            }
+            else { CreateSaveIfNot(); } //BugFix: Buttons Disappear when All Saves Deleted
+        }
         if(saveIndex != LastIndex)
         {
             defaultSave.stContainer = tContainer;
@@ -111,7 +156,8 @@ public class Workshop : EditorWindow
             GUI.SetNextControlName("b" + i.ToString());
             addElement(tContainer[i], i);
         }
-        GUI.EndScrollView();
+        GUI.EndScrollView(true);
+
         if (GUI.Button(new Rect(Screen.width - 45, 2, 75, 25), "Clear", clearButtonGUIStyle))
         {
             inactiveElements.Clear();
@@ -127,15 +173,16 @@ public class Workshop : EditorWindow
         if (!isAdding) { dragg();}
         tempInt = (tContainer.Count == 0) ? tContainer.Count : tContainer.Count - 1;
         if (isAdding) {
+            scrollPosition = GUI.BeginScrollView(new Rect(0, 20, Screen.width, Screen.height - 60), scrollPosition, new Rect(0, 0, Screen.width, tContainer.Count * 25 + 30), false, false, GUIStyle.none, GUI.skin.verticalScrollbar);  //Bug Fix: Scroll doesn't include the TextField
             GUI.SetNextControlName("t" + (tempInt).ToString());
-            tContainer[tempInt] = GUI.TextField(new Rect(10, 25 * tempInt + 30, Screen.width - 20, 20), tContainer[tempInt], 40);
+            tContainer[tempInt] = GUI.TextField(new Rect(10, 25 * tempInt + 30, Screen.width - 50, 20), tContainer[tempInt], 40);
+            GUI.EndScrollView(true);
             GUI.FocusControl("t" + (tempInt).ToString());
-            
         }
-        
+
         if (isAdding && Event.current.keyCode == KeyCode.Return && !(Event.current.type == EventType.KeyUp))
         {
-            if (GameObject.Find(tContainer[tempInt]) == null || inactiveElements.ContainsKey(tContainer[tempInt])) { tContainer.RemoveAt(tempInt);}
+            if (GameObject.Find(tContainer[tempInt]) == null && !inactiveElements.ContainsKey(tContainer[tempInt])) { tContainer.RemoveAt(tempInt);} //BugFix: Inactive buttons Removed after Enter Hit
             else
             {
                 GUI.SetNextControlName("b" + (tempInt).ToString());
@@ -144,7 +191,9 @@ public class Workshop : EditorWindow
                 Repaint();
             }
         }
-
+        
+        if (GUI.Button(new Rect(10, Screen.height - 42, 85, 20), "By TensaDev", EditorStyles.whiteBoldLabel)) { Help.BrowseURL("https://github.com/redamazoz");}
+        
     }
     void addElement(string elementName, int i) {
 
@@ -159,12 +208,25 @@ public class Workshop : EditorWindow
         }
         else if(inactiveElements.ContainsKey(elementName))
         {
-            if (!((inactiveElements[elementName]).activeSelf))
+            //BugFix: Buttons Disappear when Inactive Element in List Deleted in Hierarchy
+            if (inactiveElements[elementName] == null)
+            {
+                if (GUI.Button(new Rect(10, 25 * i + 30, Screen.width - 35, 20), "", offButtonGUIStyle)) { Selection.activeGameObject = null; }
+                if (GUI.Button(new Rect(Screen.width - 35, 25 * i + 30, 20, 20), (Texture)AssetDatabase.LoadAssetAtPath("Assets/Workshop/Editor/x.png", typeof(Texture)), EditorStyles.miniButton)) { tContainer.RemoveAt(i); }
+            }
+            else if (!((inactiveElements[elementName]).activeSelf))
             {
                 if (GUI.Button(new Rect(10, 25 * i + 30, Screen.width - 35, 20), elementName, offButtonGUIStyle)) { Selection.activeGameObject = inactiveElements[elementName];}
                 if (GUI.Button(new Rect(Screen.width - 35, 25 * i + 30, 20, 20), (Texture)AssetDatabase.LoadAssetAtPath("Assets/Workshop/Editor/x.png", typeof(Texture)), EditorStyles.miniButton)) { tContainer.RemoveAt(i);}
             }
         }
+        //BugFix: No Delete Button if GameObjs isn't in LoadedScenes or is Deleted from Hierarchy
+        else if (elementName != "")
+        {
+            if (GUI.Button(new Rect(10, 25 * i + 30, Screen.width - 35, 20), "", onButtonGUIStyle)) { Selection.activeGameObject = null; }
+            if (GUI.Button(new Rect(Screen.width - 35, 25 * i + 30, 20, 20), (Texture)AssetDatabase.LoadAssetAtPath("Assets/Workshop/Editor/x.png", typeof(Texture)), EditorStyles.miniButton)) { tContainer.RemoveAt(i); }
+        }
+
     }
 
     private void dragg()
@@ -175,32 +237,54 @@ public class Workshop : EditorWindow
         {
             // Show a copy icon on the drag
             DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-            //Debug.Log((DragAndDrop.objectReferences[0]).name);
+
             if (eventType == EventType.DragPerform && (DragAndDrop.objectReferences[0]).name != "")
             {
                 GUI.SetNextControlName("t" + (tempInt).ToString());
-                tContainer.Add((DragAndDrop.objectReferences[0]).name);
+                for (int i = 0; i < DragAndDrop.objectReferences.Length; i++)
+                {
+                    tContainer.Add((DragAndDrop.objectReferences[i]).name);
+                }
+
                 DragAndDrop.AcceptDrag();
-                inactiveElements[DragAndDrop.objectReferences[0].name] = (GameObject)(DragAndDrop.objectReferences[0]);
-                if (inactiveElements[DragAndDrop.objectReferences[0].name].activeSelf) { inactiveElements.Remove(DragAndDrop.objectReferences[0].name); }
+                //Making Stack of Elements Draggable at Once
+                for(int i = 0; i < DragAndDrop.objectReferences.Length; i++)
+                {
+                    inactiveElements[DragAndDrop.objectReferences[i].name] = (GameObject)(DragAndDrop.objectReferences[i]);
+                    if (inactiveElements[DragAndDrop.objectReferences[i].name].activeSelf) { inactiveElements.Remove(DragAndDrop.objectReferences[i].name);}
+                }
             }
             Event.current.Use();
         }
+    }
+
+    //BugFix: Buttons Disappear when All Saves Deleted
+    private void CreateSaveIfNot()
+    {
+        defaultSave = CreateInstance<WorkshopElementsScriptableObj>();
+        AssetDatabase.CreateAsset(defaultSave, "Assets/Workshop/Editor/Saves/defaultSave.asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 
     private void GetSceneInactiveGameObjs(Dictionary<string, GameObject> sceneInactiveGO)
     {
         List<GameObject> inactiveGO = new List<GameObject>();
 
-        //for Active Scene
-        SceneManager.GetActiveScene().GetRootGameObjects(inactiveGO);
-
-        foreach (GameObject go in inactiveGO)
+        //for Every Scene in Hierarchy
+        for(int i =0; i < SceneManager.sceneCount; i++)
         {
-            foreach (Transform child in go.GetComponentsInChildren<Transform>(true) ) //Include Inactive Childs
+            SceneManager.GetSceneAt(i).GetRootGameObjects(inactiveGO);
+
+            foreach (GameObject go in inactiveGO)
             {
-                if (!(child.gameObject.activeSelf)) { sceneInactiveGO[child.gameObject.name] = child.gameObject;}
+                foreach (Transform child in go.GetComponentsInChildren<Transform>(true)) //Include Inactive Childs
+                {
+                    if (!(child.gameObject.activeSelf)) { sceneInactiveGO[child.gameObject.name] = child.gameObject; }
+                }
             }
         }
+        
     }
+
 }
